b8dcb97a99c69539ae2e975a7d78065a34f42ee8
nonicalx <nonicalx@gmail.com>
2024-10-28 22:56:24 +0000

    Add keydown function

--- drum kit/drum_kit.js
+++ drum kit/drum_kit.js
@@ -1,35 +1,41 @@
 //Created by Nonso Oranye in 2024
 
 const KEYS = [
-  { label: "a", name: "clap" },
-  { label: "s", name: "hihat" },
-  { label: "d", name: "kick" },
-  { label: "f", name: "openhat" },
-  { label: "g", name: "boom" },
-  { label: "h", name: "ride" },
-  { label: "j", name: "snare" },
-  { label: "k", name: "tom" },
-  { label: "l", name: "tink" },
+  { label: "a", name: "clap", src: "sounds/clap.wav" },
+  { label: "s", name: "hihat", src: "sounds/hihat.wav" },
+  { label: "d", name: "kick", src: "sounds/kick.wav" },
+  { label: "f", name: "openhat", src: "sounds/openhat.wav" },
+  { label: "g", name: "boom", src: "sounds/boom.wav" },
+  { label: "h", name: "ride", src: "sounds/ride.wav" },
+  { label: "j", name: "snare", src: "sounds/snare.wav" },
+  { label: "k", name: "tom", src: "sounds/tom.wav" },
+  { label: "l", name: "tink", src: "sounds/tink.wav" },
 ];
 
 const platform = document.getElementById("platform");
 
 for (let index = 0; index < KEYS.length; index++) {
   const key = KEYS[index];
+  const { label, name } = key;
 
   platform.insertAdjacentHTML(
     "beforeend",
     `<li>
-        <button onclick="handlePlay(event)">
-            <span>${key.label}</span>
-            <span>${key.name}</span>
+        <button id="Key${label.toUpperCase()}" onclick="handlePlay(event)">
+            <span>${label}</span>
+            <span>${name}</span>
         </button>
     </li>`
   );
 }
 
 function handlePlay(event) {
-  event.currentTarget.classList.add("playing");
-  setTimeout(() => {}, 1000);
-  event.currentTarget.classList.remove("playing");
+  event.target.classList.add("playing");
+  setTimeout(() => {
+    event.target.classList.remove("playing");
+  }, 500);
 }
+
+window.addEventListener("keydown", function (e) {
+  this.document.getElementById(`${e.code}`).click();
+});
